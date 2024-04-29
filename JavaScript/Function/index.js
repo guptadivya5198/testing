40ba3c37d2a40bc551cb8d8e2a6e7f8bddfa489c
guptadivya5198 <guptadivya5198@gmail.com>
2024-04-29 13:59:40 +0530

    abcd

--- JavaScript/Function/index.js
+++ JavaScript/Function/index.js
@@ -24,3 +24,14 @@ let stand2 = function(){
  
 let jump =  stand2;
 jump();
+
+
+function sum(){
+   let total =0;
+    for(let value of arguments)
+    total= total+ value;
+    return total;
+}
+
+let ans = sum(1,2,3,7,4,5,5,6,7,);
+console.log(ans);
