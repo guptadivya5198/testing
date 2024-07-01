08881f3c59d5024a2996f24a4bda53a9016dcc5c
guptadivya5198 <guptadivya5198@gmail.com>
2024-07-01 22:15:20 +0530

    abcd

--- dsa/Pattern Based Qun's/Ptr.java
+++ dsa/Pattern Based Qun's/Ptr.java
@@ -60,11 +60,11 @@ public class Ptr
         while(i <=n){
             int j =1;
             while(j<=i){
-                String ch=(String) ('A' + i- 1  + " " );
-                System.out.print(ch);
+                char ch=(char) ('A' + i- 1) ;
+                System.out.print(ch+" ");
                 j++;
             }
-            System.out.println( );
+            System.out.println();
             i++;
 
         }
