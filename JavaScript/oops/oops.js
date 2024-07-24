3d912ed75b51a6d4a17a384a7ef1e9fcf9deaf11
guptadivya5198 <guptadivya5198@gmail.com>
2024-07-24 21:44:31 +0530

    bshsj

--- JavaScript/oops/oops.js
+++ JavaScript/oops/oops.js
@@ -8,5 +8,39 @@
 
  const userOne = new User('hitesh', 12, true);
  const usertwo = new User('harry', 11, false);
- console.log(userOne);
- console.log(usertwo);
+//  console.log(userOne);
+//  console.log(usertwo);
+
+//  prototype
+
+function creatUser(username, score){
+  this.username = username,
+  this.score =  score
+}
+
+creatUser.prototype.increment = function(){
+    this.score++
+}
+creatUser.prototype.printMe = function(){
+   console.log(`price is ${this.score}`);
+}
+const chai =new creatUser('chai' ,25)
+const tea = new creatUser('tea',125)
+
+chai.printMe()
+
+let myHeros =["thor", "spiderman"]
+
+let heroPower = {
+   thor:'hammer',
+   spiderman: 'sling',
+
+   getSpiderPwer : function(){
+      console.log(`spidy power is ${this.spiderman}`);
+   }
+}
+Object.prototype.hitesh = function(){
+   console.log(`hitesh is present in all object`);
+}
+// heroPower.hitesh()
+myHeros.hitesh()
