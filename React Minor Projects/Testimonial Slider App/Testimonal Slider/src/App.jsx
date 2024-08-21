35f0a70ffbe5957f13e6218f4e2476123b013fda
guptadivya5198 <guptadivya5198@gmail.com>
2024-08-21 13:18:33 +0530

    abcd

--- React Minor Projects/Testimonial Slider App/Testimonal Slider/src/App.jsx
+++ React Minor Projects/Testimonial Slider App/Testimonal Slider/src/App.jsx
@@ -7,9 +7,9 @@ function App() {
     <div className="flex flex-col h-[100vh] w-[100vw] justify-center items-center bg-gray-200">
       <div className="text-center">
         <h1 className="text-4xl font-bold">Our Testimonial</h1>
+        <div className="bg-violet-400 h-[4px] w-1/5 mt-1 mx-auto"></div>
+        <Testimonial reviews={reviews} />
       </div>
-      <div className="bg-violet-400 h-[4px] w-[1/5] mt-1"></div>
-      <Testimonial reviews={reviews} />
     </div>
   );
 }
--- React Minor Projects/Testimonial Slider App/Testimonal Slider/src/Component/Card.jsx
+++ React Minor Projects/Testimonial Slider App/Testimonal Slider/src/Component/Card.jsx
@@ -1,35 +1,32 @@
 import React from 'react';
 import { FaQuoteLeft, FaQuoteRight } from 'react-icons/fa';
+
 function Card(props) {
   let review = props.review;
   return (
-    <div>
-      <div>
-        <img src={review.image} />
+    <div className="flex flex-col md:relative">
+      <div className="absolute top-[-7rem] z-[10] mx-auto">
+        <img
+          className="aspect-square rounded-full w-[140px] h-[140px] z-25"
+          src={review.image}
+        />
+        <div className="rounded-full w-[140px] h-[140px] z-[-10] bg-violet-500 absolute top-[-6px] left-[10px]"></div>
       </div>
 
-      <div>
-        <p>{review.name}</p>
+      <div className="text-center mt-7">
+        <p className="font-bold tracking-wide text-2xl capitalize">
+          {review.name}
+        </p>
+        <p className="text-violet-300 uppercase text-sm">{review.job}</p>
       </div>
 
-      <div>
-        <p>{review.job}</p>
-      </div>
-
-      <div>
+      <div className="text-violet-400 mx-auto mt-5">
         <FaQuoteLeft />
       </div>
 
-      <div>{review.text}</div>
-      <div>
+      <div className="mt-4 text-center text-slate-500">{review.text}</div>
+      <div className="text-violet-400 mx-auto mt-5">
         <FaQuoteRight />
-      </div>
-      <div>
-        <button>+</button>
-        <button>*</button>
-      </div>
-      <div>
-        <button>Surprise Me</button>
       </div>
     </div>
   );
